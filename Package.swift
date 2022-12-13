49eb2af3ac13b2e7e32a63e131ec93450f1f547f
Mikhail Dudarev <dev@mikhaildudarev.com>
2022-12-13 21:43:04 +0300

    set dynamic library type

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Keychain",
+            type: .dynamic,
             targets: ["Keychain"]),
     ],
     dependencies: [
